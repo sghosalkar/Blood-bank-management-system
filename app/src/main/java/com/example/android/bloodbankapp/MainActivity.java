38f37280756a3c7cb31d3b54621afb137ebeb5a3
Shubham G <shubhamghosalkar.sg@gmail.com>
2017-03-16 00:20:31 +0530

    first commit

---
+++ app/src/main/java/com/example/android/bloodbankapp/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.android.bloodbankapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_new_entry:
+                    mTextMessage.setText(R.string.title_new_entry);
+                    return true;
+                case R.id.navigation_transactions:
+                    mTextMessage.setText(R.string.title_transactions);
+                    return true;
+                case R.id.navigation_statistics:
+                    mTextMessage.setText(R.string.title_statistics);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
