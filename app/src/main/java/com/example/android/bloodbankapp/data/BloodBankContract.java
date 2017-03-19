d599af90f9b5fb42b95dc22d5cee3fdd31165c22
Shubham G <shubhamghosalkar.sg@gmail.com>
2017-03-19 11:47:03 +0530

    Add database contract

--- app/src/main/java/com/example/android/bloodbankapp/data/BloodBankContract.java
+++ app/src/main/java/com/example/android/bloodbankapp/data/BloodBankContract.java
@@ -1,5 +1,6 @@
 package com.example.android.bloodbankapp.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -21,21 +22,68 @@ public class BloodBankContract {
     public static final class DonorEntry implements BaseColumns {
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_DONOR).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DONOR;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DONOR;
+
+        public static final String TABLE_NAME = "donor";
+
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_CONTACT_NO = "contact_no";
+        public static final String COLUMN_BLOOD_GROUP = "blood_group";
+
     }
 
     public static final class ReceiverEntry implements BaseColumns {
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_RECEIVER).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_RECEIVER;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_RECEIVER;
+
+        public static final String TABLE_NAME = "receiver";
+
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_CONTACT_NO = "contact_no";
     }
 
     public static final class DonorTransactionEntry implements BaseColumns {
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_DONOR_TRANSACTION).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DONOR_TRANSACTION;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DONOR_TRANSACTION;
+
+        public static final String TABLE_NAME = "donor_transaction";
+
+        public static final String COLUMN_DONOR_KEY = "donor_id";
+        public static final String COLUMN_QUANTITY = "quantity";
+        public static final String COLUMN_PRICE = "price";
+        public static final String COLUMN_TRANSACTION_DATE = "date";
     }
 
     public static final class ReceiverTransactionEntry implements BaseColumns {
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_RECEIVER_TRANSACTION).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_RECEIVER_TRANSACTION;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_RECEIVER_TRANSACTION;
+
+        public static final String TABLE_NAME = "receiver_transaction";
+
+        public static final String COLUMN_RECEIVER_KEY = "receiver_id";
+        public static final String COLUMN_BLOOD_GROUP = "blood_group";
+        public static final String COLUMN_QUANTITY = "quantity";
+        public static final String COLUMN_PRICE = "price";
+        public static final String COLUMN_TRANSACTION_DATE = "date";
     }
 
 }
