fc8c956f53c7fe890dae2e873afa591ad7524217
SoniaChhabria <chhabriasonia05@gmail.com>
2017-03-22 14:45:42 +0530

    Add report layout in statistics

---
+++ app/src/main/java/com/example/android/bloodbankapp/ReportAdapter.java
@@ -0,0 +1,84 @@
+package com.example.android.bloodbankapp;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.Inflater;
+
+/**
+ * Created by DELL on 19-Mar-17.
+ */
+
+public class ReportAdapter extends ArrayAdapter {
+   private List list=new ArrayList();
+
+    public ReportAdapter(@NonNull Context context, @LayoutRes int resource) {
+        super(context, resource);
+    }
+
+
+    public void add(Report object) {
+     list.add(object);
+        super.add(object);
+    }
+
+    static class ReportHolder{
+        TextView month;
+        ImageView up;
+        TextView bldqtyin;
+        ImageView down;
+        TextView bldqtyout;
+    }
+    @Override
+    public int getCount() {
+        return this.list.size();
+    }
+
+    @Nullable
+    @Override
+    public Object getItem(int position) {
+        return this.list.get(position);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row;
+        row=convertView;
+        ReportHolder holder;
+        if(convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(R.layout.row_layout, parent, false);
+            holder = new ReportHolder();
+            holder.month = (TextView) row.findViewById(R.id.tv_month);
+            holder.up = (ImageView) row.findViewById(R.id.iv_out);
+            holder.bldqtyin = (TextView) row.findViewById(R.id.tv_in);
+            holder.down = (ImageView) row.findViewById(R.id.iv_in);
+            holder.bldqtyout = (TextView) row.findViewById(R.id.tv_out);
+            row.setTag(holder);
+        }
+        else{
+            holder= (ReportHolder) row.getTag();
+
+
+        }
+        Report r= (Report) getItem(position);
+        holder.month.setText(r.getMonth());
+        holder.down.setImageResource(r.getBldin());
+        holder.bldqtyin.setText(r.getBldqtyin());
+        holder.up.setImageResource(r.getBldout());
+        holder.bldqtyout.setText(r.getBldqtyout());
+
+        return row;
+    }
+}
--- app/src/main/java/com/example/android/bloodbankapp/StatisticsReportsFragment.java
+++ app/src/main/java/com/example/android/bloodbankapp/StatisticsReportsFragment.java
@@ -1,19 +1,49 @@
 package com.example.android.bloodbankapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 
 public class StatisticsReportsFragment extends Fragment {
-
+ ListView listview;
+    int up=R.drawable.up;
+    int down=R.drawable.down;
+    String month[]={"December","January","February","MArch"};
+    String bldqtyin[]={"123 units","578 units","456 units","698 units"};
+    String bldqtyout[]={"123 units","578 units","456 units","698 units"};
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=  inflater.inflate(R.layout.fragment_statistics_reports, container, false);
+         listview= (ListView) view.findViewById(R.id.lv_report);
+        ReportAdapter ra=new ReportAdapter(getContext(),R.layout.row_layout);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent=new Intent(getActivity(),BloodReportDetails.class);
+                intent.putExtra("Month",listview.getItemAtPosition(i).toString());
+                startActivity(intent);
+            }
+        });
+        int i=0;
+        for(String trace:month){
+            Report r=new Report(trace,down,bldqtyin[i],up,bldqtyout[i]);
+            ra.add(r);
+            i++;
+        }
+
+        listview.setAdapter(ra);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics_reports, container, false);
+        return view;
     }
+
+
+
 }
