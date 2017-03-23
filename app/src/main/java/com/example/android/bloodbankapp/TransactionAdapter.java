f5e85a0087d78691636bc2f3a9993def366f3a7e
Shubham G <shubhamghosalkar.sg@gmail.com>
2017-03-23 15:57:57 +0530

    Add RecyclerView to inflate dummy list

--- app/src/main/java/com/example/android/bloodbankapp/TransactionAdapter.java
+++ app/src/main/java/com/example/android/bloodbankapp/TransactionAdapter.java
@@ -1,51 +1,62 @@
 package com.example.android.bloodbankapp;
 
 import android.content.Context;
-import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CursorAdapter;
 import android.widget.TextView;
 
 /**
  * Created by shubham on 22/3/17.
  */
 
-public class TransactionAdapter extends CursorAdapter {
+public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>{
 
-    public TransactionAdapter(Context context, Cursor c, int flags) {
-        super(context, c, flags);
+    private int mNumberItems;
+
+    public TransactionAdapter(int numberOfItems) {
+        mNumberItems = numberOfItems;
     }
 
     @Override
-    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_transaction, viewGroup, false);
-        ViewHolder viewHolder = new ViewHolder(view);
-        view.setTag(viewHolder);
-        return view;
+    public TransactionAdapter.TransactionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.list_item_transaction, parent, false);
+        TransactionViewHolder viewHolder = new TransactionViewHolder(view);
+        return viewHolder;
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        ViewHolder viewHolder = (ViewHolder) view.getTag();
-        viewHolder.nameView.setText("Mogambo");
-        viewHolder.typeView.setText("Donor");
-        viewHolder.bloodGroupView.setText("O+");
-        viewHolder.quantityView.setText("300 mL");
+    public void onBindViewHolder(TransactionAdapter.TransactionViewHolder holder, int position) {
+        holder.bind(position);
     }
 
-    public static class ViewHolder {
+    @Override
+    public int getItemCount() {
+        return mNumberItems;
+    }
+
+    class TransactionViewHolder extends RecyclerView.ViewHolder {
         public final TextView nameView;
         public final TextView typeView;
         public final TextView bloodGroupView;
         public final TextView quantityView;
 
-        public ViewHolder(View view) {
+        public TransactionViewHolder(View view) {
+            super(view);
             nameView = (TextView) view.findViewById(R.id.list_item_name_textview);
             typeView = (TextView) view.findViewById(R.id.list_item_type_textview);
             bloodGroupView = (TextView) view.findViewById(R.id.list_item_blood_group_textview);
             quantityView = (TextView) view.findViewById(R.id.list_item_quantity_textview);
+        }
+
+        void bind(int listIndex) {
+            nameView.setText("Mogambo khush hua");
+            typeView.setText("Donor");
+            bloodGroupView.setText("O+");
+            quantityView.setText("300 mL");
         }
     }
 }
--- app/src/main/java/com/example/android/bloodbankapp/TransactionsFragment.java
+++ app/src/main/java/com/example/android/bloodbankapp/TransactionsFragment.java
@@ -2,38 +2,38 @@ package com.example.android.bloodbankapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
 
 public class TransactionsFragment extends Fragment {
 
     public static final String LOG_TAG = TransactionsFragment.class.getSimpleName();
+    private static final int TRANS_LIST_ITEMS = 100;
     private TransactionAdapter mTransactionAdapter;
-    private ListView mListView;
+    private RecyclerView mTransactionList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        mTransactionAdapter = new TransactionAdapter(getActivity(), null, 0);
-
         View rootView = inflater.inflate(R.layout.fragment_transactions, container, false);
 
-        mListView = (ListView) rootView.findViewById(R.id.listview_transactions);
-        mListView.setAdapter(mTransactionAdapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast toast = Toast.makeText(getContext(), "Here", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
-
+        mTransactionList = (RecyclerView) rootView.findViewById(R.id.recyclerview_transactions);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext());
+        mTransactionList.setLayoutManager(layoutManager);
+        mTransactionList.setHasFixedSize(true);
+        mTransactionAdapter = new TransactionAdapter(TRANS_LIST_ITEMS);
+        mTransactionList.setAdapter(mTransactionAdapter);
+//        mTransactionAdapter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Toast toast = Toast.makeText(getContext(), "Here", Toast.LENGTH_SHORT);
+//                toast.show();
+//            }
+//        });
         return rootView;
     }
 }
