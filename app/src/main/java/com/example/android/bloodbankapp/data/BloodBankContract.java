772bf9d4a206a0fcfe395a3e6f036339dd7f1b24
SoniaChhabria <chhabriasonia05@gmail.com>
2017-03-28 10:41:50 +0530

    Database Schema Updated

--- app/src/main/java/com/example/android/bloodbankapp/data/BloodBankContract.java
+++ app/src/main/java/com/example/android/bloodbankapp/data/BloodBankContract.java
@@ -16,9 +16,9 @@ public class BloodBankContract {
 
     public static final String PATH_DONOR = "donor";
     public static final String PATH_RECEIVER = "receiver";
-    public static final String PATH_DONOR_TRANSACTION = "donor_transaction";
-    public static final String PATH_RECEIVER_TRANSACTION = "receiver_transaction";
-
+    //public static final String PATH_DONOR_TRANSACTION = "donor_transaction";
+    //public static final String PATH_RECEIVER_TRANSACTION = "receiver_transaction";
+    public static final String PATH_DATE_TABLE="date_table";
     public static final class DonorEntry implements BaseColumns {
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_DONOR).build();
@@ -33,7 +33,9 @@ public class BloodBankContract {
         public static final String COLUMN_NAME = "name";
         public static final String COLUMN_CONTACT_NO = "contact_no";
         public static final String COLUMN_BLOOD_GROUP = "blood_group";
-
+        public static final String COLUMN_QUANTITY = "quantity";
+        public static final String COLUMN_PRICE = "price";
+        public static final String COLUMN_DATE_ID = "date_key";
     }
 
     public static final class ReceiverEntry implements BaseColumns {
@@ -49,8 +51,29 @@ public class BloodBankContract {
 
         public static final String COLUMN_NAME = "name";
         public static final String COLUMN_CONTACT_NO = "contact_no";
+        public static final String COLUMN_BLOOD_GROUP = "blood_group";
+        public static final String COLUMN_QUANTITY = "quantity";
+        public static final String COLUMN_PRICE = "price";
+        public static final String COLUMN_DATE_ID = "date_key";
+
     }
 
+    public static final class DateTable implements BaseColumns{
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_DATE_TABLE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DATE_TABLE;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DATE_TABLE;
+        public static final String TABLE_NAME="date_table";
+
+        public static final String COLUMN_DATE_DATENO="day";
+        public static final String COLUMN_DATE_MONTH="month";
+        public static final String COLUMN_DATE_YEAR="year";
+
+    }
+    /*
     public static final class DonorTransactionEntry implements BaseColumns {
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_DONOR_TRANSACTION).build();
@@ -64,6 +87,7 @@ public class BloodBankContract {
 
         public static final String COLUMN_DONOR_KEY = "donor_id";
         public static final String COLUMN_QUANTITY = "quantity";
+        public static final String COLUMN_BLOOD_GROUP = "blood_group";
         public static final String COLUMN_PRICE = "price";
         public static final String COLUMN_TRANSACTION_DATE = "date";
     }
@@ -85,5 +109,5 @@ public class BloodBankContract {
         public static final String COLUMN_PRICE = "price";
         public static final String COLUMN_TRANSACTION_DATE = "date";
     }
-
+*/
 }
