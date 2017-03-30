0557e90bbbd898161340b21cb44ed0d87b8ae52b
k-slugger <shubhamghosalkar.sg@gmail.com>
2017-03-30 21:01:38 +0530

    Refactor contract and dbHelper according to new schema

--- app/src/main/java/com/example/android/bloodbankapp/data/BloodBankContract.java
+++ app/src/main/java/com/example/android/bloodbankapp/data/BloodBankContract.java
@@ -14,42 +14,23 @@ public class BloodBankContract {
 
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
-    public static final String PATH_DONOR = "donor";
-    public static final String PATH_RECEIVER = "receiver";
-    public static final String PATH_DATE_TABLE="date_table";
+    public static final String PATH_TRANSACTION = "transactions";
+    public static final String PATH_DATE = "date_table";
 
-    public static final class DonorEntry implements BaseColumns {
+    public static final class TransactionEntry implements BaseColumns {
         public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_DONOR).build();
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_TRANSACTION).build();
 
         public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DONOR;
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TRANSACTION;
         public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DONOR;
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TRANSACTION;
 
-        public static final String TABLE_NAME = "donor";
+        public static final String TABLE_NAME = "transactions";
 
         public static final String COLUMN_NAME = "name";
         public static final String COLUMN_CONTACT_NO = "contact_no";
-        public static final String COLUMN_BLOOD_GROUP = "blood_group";
-        public static final String COLUMN_QUANTITY = "quantity";
-        public static final String COLUMN_PRICE = "price";
-        public static final String COLUMN_DATE_KEY = "date_id";
-    }
-
-    public static final class ReceiverEntry implements BaseColumns {
-        public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_RECEIVER).build();
-
-        public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_RECEIVER;
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_RECEIVER;
-
-        public static final String TABLE_NAME = "receiver";
-
-        public static final String COLUMN_NAME = "name";
-        public static final String COLUMN_CONTACT_NO = "contact_no";
+        public static final String COLUMN_TYPE = "type";
         public static final String COLUMN_BLOOD_GROUP = "blood_group";
         public static final String COLUMN_QUANTITY = "quantity";
         public static final String COLUMN_PRICE = "price";
@@ -58,12 +39,12 @@ public class BloodBankContract {
 
     public static final class DateEntry implements BaseColumns{
         public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_DATE_TABLE).build();
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_DATE).build();
 
         public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DATE_TABLE;
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DATE;
         public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DATE_TABLE;
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_DATE;
         public static final String TABLE_NAME="date_table";
 
         public static final String COLUMN_DAY = "day";
