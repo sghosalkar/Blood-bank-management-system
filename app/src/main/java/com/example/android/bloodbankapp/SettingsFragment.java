ff350ccc551cabe7fb7da0603bdb7e01c4916225
Jaihinduja <jaihinduja8@gmail.com>
2017-03-31 16:08:12 +0530

    login and signup

--- app/src/main/java/com/example/android/bloodbankapp/SettingsFragment.java
+++ app/src/main/java/com/example/android/bloodbankapp/SettingsFragment.java
@@ -2,14 +2,15 @@ package com.example.android.bloodbankapp;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-
 
 public class SettingsFragment extends PreferenceFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         addPreferencesFromResource(R.xml.pref_general);
     }
