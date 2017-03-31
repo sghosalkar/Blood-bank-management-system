8d729d10d5a4271dc7bd642d1797fa38c9563738
Jaihinduja <jaihinduja8@gmail.com>
2017-03-31 16:11:15 +0530

    login activity

--- app/src/main/java/com/example/android/bloodbankapp/LoginActivity.java
+++ app/src/main/java/com/example/android/bloodbankapp/LoginActivity.java
@@ -1,13 +1,50 @@
 package com.example.android.bloodbankapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import static android.R.attr.onClick;
+
+public class LoginActivity extends Activity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button login = (Button) findViewById(R.id.login);
+        Button signup = (Button) findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),SignupActivity.class));
+
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText user = (EditText) findViewById(R.id.user);
+                EditText pass = (EditText) findViewById(R.id.pass);
+                if (user.getText().toString().equals("abc") && pass.getText().toString().equals("abc")) {
+                    Toast.makeText(getApplicationContext(), "login successfull", Toast.LENGTH_LONG).show();
+
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
-}
+
+
+    }
+
