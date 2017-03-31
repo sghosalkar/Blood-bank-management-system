108cf2b239735925c5d52434a0c658900bfd4192
SoniaChhabria <chhabriasonia05@gmail.com>
2017-03-31 22:34:57 +0530

    Donor and receiver database queries along with changes in xml file

--- app/src/main/java/com/example/android/bloodbankapp/NewEntryDonorFragment.java
+++ app/src/main/java/com/example/android/bloodbankapp/NewEntryDonorFragment.java
@@ -1,14 +1,26 @@
 package com.example.android.bloodbankapp;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.android.bloodbankapp.R;
+import com.example.android.bloodbankapp.data.BloodBankContract;
+import com.example.android.bloodbankapp.data.BloodBankDbHelper;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by jaihi on 3/27/2017.
@@ -16,11 +28,114 @@ import com.example.android.bloodbankapp.R;
 
 public class NewEntryDonorFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
+    EditText et_dname, et_dphoneno,et_dquantity,et_price;
+    Spinner sp_dbloodgroup;
+    Button btn_save;
+
+    SQLiteDatabase mdb;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_entry_donor, container, false);
+       View view= inflater.inflate(R.layout.fragment_new_entry_donor, container, false);
+        et_dname= (EditText) view.findViewById(R.id.et_dname);
+        et_dphoneno= (EditText) view.findViewById(R.id.et_dphone);
+        et_dquantity= (EditText) view.findViewById(R.id.et_dquantity);
+        et_price= (EditText) view.findViewById(R.id.et_dprice);
+        sp_dbloodgroup= (Spinner) view.findViewById(R.id.sp_bldgrp);
+        btn_save= (Button) view.findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                 String dname = et_dname.getText().toString();
+                 String dphoneno = et_dphoneno.getText().toString();
+                 Integer quantity = Integer.parseInt(et_dquantity.getText().toString());
+                 Integer price = Integer.parseInt(et_price.getText().toString());
+                 String bldgrp = sp_dbloodgroup.getSelectedItem().toString();
+                if (dname.length() == 0 || dphoneno.length() !=10 || quantity < 0 || price  < 0)
+                    Toast.makeText(getContext(), "Please Enter Details", Toast.LENGTH_SHORT).show();
+               else {
+                    BloodBankDbHelper db=new BloodBankDbHelper(getActivity());
+                    mdb=db.getWritableDatabase();
+                    addDonor(dname,dphoneno,quantity,price,"donor",bldgrp);
+                Toast.makeText(getContext(), "Data Saved", Toast.LENGTH_SHORT).show();
+
+
+            }}
+        });
+
+
+
+
+        return view;
+    }
+
+    private void addDonor(String dname, String dphoneno, Integer quantity, Integer price, String type, String bldgrp) {
+
+        String Query=" Select "+ BloodBankContract.DateEntry._ID + " , " + BloodBankContract.DateEntry.COLUMN_DAY
+                + " , "+BloodBankContract.DateEntry.COLUMN_MONTH + " , " + BloodBankContract.DateEntry.COLUMN_YEAR +
+                " from "+BloodBankContract.DateEntry.TABLE_NAME +" ; ";
+
+        Cursor c=mdb.rawQuery(Query,null);
+        c.moveToFirst();
+        int a = -1;
+        int dateid = 0;
+        if (c.getCount() > 0 && c!=null){
+            Calendar calender = Calendar.getInstance();
+            SimpleDateFormat df=new SimpleDateFormat("dd-MM-yyyy");
+            String currentDate = df.format(calender.getTime());
+            String current[] = currentDate.split("-");
+            int currentDay = Integer.parseInt(current[0]);
+            int currentMonth = Integer.parseInt(current[1]);
+            int currentYear=Integer.parseInt(current[2]);
+            for(int i=0;i<c.getCount();i++) {
+                if (c.getInt(1) == currentDay && c.getInt(2) == currentMonth && c.getInt(3) == currentYear) {
+                    a = c.getInt(0);
+
+                }
+                c.moveToNext();
+            }
+            if(a == -1){
+                ContentValues val=new ContentValues();
+                val.put(BloodBankContract.DateEntry.COLUMN_DAY,currentDay);
+                val.put(BloodBankContract.DateEntry.COLUMN_MONTH,currentMonth);
+                val.put(BloodBankContract.DateEntry.COLUMN_YEAR,currentYear);
+                mdb.insert(BloodBankContract.DateEntry.TABLE_NAME,null,val);
+                String q="Select "+BloodBankContract.DateEntry._ID + " from " + BloodBankContract.DateEntry.TABLE_NAME +
+                        " where "+BloodBankContract.DateEntry.COLUMN_DAY+" = "+currentDay +
+                        " and "+BloodBankContract.DateEntry.COLUMN_MONTH+" = "+currentMonth+
+                        " and "+BloodBankContract.DateEntry.COLUMN_YEAR+" = "+currentYear+" ; ";
+                Cursor cdate=mdb.rawQuery(q,null);
+                dateid=cdate.getInt(0);
+                Log.d("bb",dateid+"datemilgaya");
+
+            }
+            else
+                dateid=a;
+
+
+
+
+        }
+
+
+        ContentValues values=new ContentValues();
+        values.put(BloodBankContract.TransactionEntry.COLUMN_NAME,dname);
+        values.put(BloodBankContract.TransactionEntry.COLUMN_CONTACT_NO,dphoneno);
+        values.put(BloodBankContract.TransactionEntry.COLUMN_TYPE,type);
+        values.put(BloodBankContract.TransactionEntry.COLUMN_BLOOD_GROUP,bldgrp);
+        values.put(BloodBankContract.TransactionEntry.COLUMN_QUANTITY,quantity);
+        values.put(BloodBankContract.TransactionEntry.COLUMN_PRICE,price);
+        values.put(BloodBankContract.TransactionEntry.COLUMN_DATE_KEY,dateid);
+        long rid = mdb.insert(BloodBankContract.TransactionEntry.TABLE_NAME,null,values);
+        if (rid<0)
+            Log.d("bb","issue");
+        else{
+            Log.d("bb","inserted");
+        Log.d("bb",dateid+"bjm");
+        }
 
     }
 
@@ -33,5 +148,8 @@ public class NewEntryDonorFragment extends Fragment implements AdapterView.OnIte
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+
+
 }
 
--- app/src/main/java/com/example/android/bloodbankapp/NewEntryReceiverFragment.java
+++ app/src/main/java/com/example/android/bloodbankapp/NewEntryReceiverFragment.java
@@ -1,12 +1,25 @@
 package com.example.android.bloodbankapp;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.android.bloodbankapp.R;
+import com.example.android.bloodbankapp.data.BloodBankContract;
+import com.example.android.bloodbankapp.data.BloodBankDbHelper;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by jaihi on 3/27/2017.
@@ -14,10 +27,115 @@ import com.example.android.bloodbankapp.R;
 
 public class NewEntryReceiverFragment extends Fragment {
 
+    EditText et_rname, et_rphoneno,et_rquantity,et_rprice;
+    Spinner sp_rdbloodgroup;
+    Button btn_rsave;
+
+    SQLiteDatabase mdb;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view=inflater.inflate(R.layout.fragment_new_entry_receiver, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_entry_receiver, container, false);
+        et_rname= (EditText) view.findViewById(R.id.et_rname);
+        et_rphoneno= (EditText) view.findViewById(R.id.et_rphone);
+        et_rquantity= (EditText) view.findViewById(R.id.et_rquantity);
+        et_rprice= (EditText) view.findViewById(R.id.et_rprice);
+        sp_rdbloodgroup= (Spinner) view.findViewById(R.id.sp_rbldgrp);
+        btn_rsave= (Button) view.findViewById(R.id.btn_rsave);
+btn_rsave.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        String rname = et_rname.getText().toString();
+        String rphoneno = et_rphoneno.getText().toString();
+        Integer quantity = Integer.parseInt(et_rquantity.getText().toString());
+        Integer price = Integer.parseInt(et_rprice.getText().toString());
+        String bldgrp = sp_rdbloodgroup.getSelectedItem().toString();
+        if (rname.length() == 0 || rphoneno.length() !=10 || quantity < 0 || price  < 0)
+            Toast.makeText(getContext(), "Please Enter Details", Toast.LENGTH_SHORT).show();
+        else {
+            BloodBankDbHelper db=new BloodBankDbHelper(getActivity());
+            mdb=db.getWritableDatabase();
+            addReceiver(rname,rphoneno,quantity,price,"receiver",bldgrp);
+            Toast.makeText(getContext(), "Data Saved", Toast.LENGTH_SHORT).show();
+
+        }}
+});
+
+
+        return view;
     }
+
+    private void addReceiver(String rname, String rphoneno, Integer quantity, Integer price, String type, String bldgrp) {
+
+        String Query=" Select "+ BloodBankContract.DateEntry._ID + " , " + BloodBankContract.DateEntry.COLUMN_DAY
+                + " , "+BloodBankContract.DateEntry.COLUMN_MONTH + " , " + BloodBankContract.DateEntry.COLUMN_YEAR +
+                " from "+BloodBankContract.DateEntry.TABLE_NAME +" ; ";
+
+        Cursor c=mdb.rawQuery(Query,null);
+        c.moveToFirst();
+        int a = -1;
+        int dateid = 0;
+        if (c.getCount() > 0 && c!=null){
+            Calendar calender = Calendar.getInstance();
+            SimpleDateFormat df=new SimpleDateFormat("dd-MM-yyyy");
+            String currentDate = df.format(calender.getTime());
+            String current[] = currentDate.split("-");
+            int currentDay = Integer.parseInt(current[0]);
+            int currentMonth = Integer.parseInt(current[1]);
+            int currentYear=Integer.parseInt(current[2]);
+            for(int i=0;i<c.getCount();i++) {
+                if (c.getInt(1) == currentDay && c.getInt(2) == currentMonth && c.getInt(3) == currentYear) {
+                    a = c.getInt(0);
+
+                }
+                c.moveToNext();
+            }
+            if(a == -1){
+                ContentValues val=new ContentValues();
+                val.put(BloodBankContract.DateEntry.COLUMN_DAY,currentDay);
+                val.put(BloodBankContract.DateEntry.COLUMN_MONTH,currentMonth);
+                val.put(BloodBankContract.DateEntry.COLUMN_YEAR,currentYear);
+                mdb.insert(BloodBankContract.DateEntry.TABLE_NAME,null,val);
+                String q="Select "+BloodBankContract.DateEntry._ID + " from " + BloodBankContract.DateEntry.TABLE_NAME +
+                        " where "+BloodBankContract.DateEntry.COLUMN_DAY+" = "+currentDay +
+                        " and "+BloodBankContract.DateEntry.COLUMN_MONTH+" = "+currentMonth+
+                        " and "+BloodBankContract.DateEntry.COLUMN_YEAR+" = "+currentYear+" ; ";
+                Cursor cdate=mdb.rawQuery(q,null);
+                 dateid=cdate.getInt(0);
+                Log.d("bb",dateid+"datemilgaya");
+
+            }
+            else
+                dateid=a;
+
+
+
+
+        }
+
+        ContentValues values=new ContentValues();
+        values.put(BloodBankContract.TransactionEntry.COLUMN_NAME,rname);
+        values.put(BloodBankContract.TransactionEntry.COLUMN_CONTACT_NO,rphoneno);
+        values.put(BloodBankContract.TransactionEntry.COLUMN_TYPE,type);
+        values.put(BloodBankContract.TransactionEntry.COLUMN_BLOOD_GROUP,bldgrp);
+        values.put(BloodBankContract.TransactionEntry.COLUMN_QUANTITY,quantity);
+        values.put(BloodBankContract.TransactionEntry.COLUMN_PRICE,price);
+        values.put(BloodBankContract.TransactionEntry.COLUMN_DATE_KEY,dateid);
+        long rid = mdb.insert(BloodBankContract.TransactionEntry.TABLE_NAME,null,values);
+        if (rid<0)
+            Log.d("bb","issue");
+        else{
+            Log.d("bb","inserted");
+        Log.d("bb",dateid+"jbjk");
+        }
+
+    }
+
+
 }
+
+
